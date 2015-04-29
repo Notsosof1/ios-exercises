7a35886bd7d8bc09e35566e43b5788e1eb5e84c5
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-04-28 17:32:07 -0700

    BlocExercises/BlocExercises/EqualityDeterminer.m

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -11,18 +11,28 @@
 @implementation EqualityDeterminer
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
+    
+    BOOL areTheyEqual = [string1 isEqualToString:string2];
     /* WORK HERE */
-    return (string1 == string2);
+    return areTheyEqual;
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
+    
+    //BOOL areTheyEqual = [number1 isEqualToValue:number2];
     /* WORK HERE */
-    return (number1 == number2);
+    return [number1 isEqualToNumber:number2];
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
-    /* WORK HERE */
-    return NO;
+    
+    //BOOL whichIntergerIsGreaterThanAnother = integer1 > integer2;
+    
+    //BOOL highIsGreater = integer:high isGreaterThan:low];
+    //NSLog(@"Is %c greater than %c? %@", integer1 > integer2 ? @"Yes" : @"No");
+    
+    //BOOL (%c "is greater than" %c), integer1, integer2 ? @"Yes":@"No");
+    return (integer1 > integer2 ? YES : NO);
 }
 
 @end
