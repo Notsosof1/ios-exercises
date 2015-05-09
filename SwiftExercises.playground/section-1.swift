296d3097b66acf03c27727bb1d41edf4c5434b70
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-05-09 15:17:34 -0700

    SwiftExerciese.playground

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -1,10 +1,10 @@
+// Playground - noun: a place where people can play
+
 import UIKit
 
-/*
 
-Strings
-
-*/
+var str = "Hello, playground"
+println(str)
 
 func favoriteCheeseStringWithCheese(cheese: String)-> String{
     let cheese = "Brie"
@@ -12,15 +12,7 @@ func favoriteCheeseStringWithCheese(cheese: String)-> String{
 }
 
 let fullSentence = favoriteCheeseStringWithCheese("cheddar")
-    println("My favorite cheese is \(fullSentence)")
-
-// Make fullSentence say "My favorite cheese is cheddar."
-
-/*
-
-Arrays & Dictionaries
-
-*/
+println("My favorite cheese is \(fullSentence).")
 
 var numberArray = [1, 2, 3, 4]
 numberArray += [5]
@@ -28,43 +20,24 @@ numberArray.append(6)
 
 numberArray.insert(9, atIndex: 0)
 
-// Add 5 to this array -   I added a few for fun
-// WORK HERE
+var numberDictionary:  [String:Int] = [
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4
+]
 
-var numberDictionary: [1: "one", 2: "two", 3: "three", 4: "four"]
+numberDictionary["five"] = 5
 numberDictionary
 
 
-var numberDictionary1: ["one": 1, "two": 2, "three": 3, "four": 4]
-numberDictionary1 ["five"] = 5
-
-
-
-// Add 5 : "five" to this dictionary
-// WORK HERE
-
-/*
-
-Loops
-
-*/
-
-// Use a closed range loop to print 1 - 10, inclusively
-// WORK HERE
+for index in 1..<10 {
+    println("\(index)");
+}
 
 for index in 1...10 {
     println("\(index)");
 }
-
-// Use a half-closed range loop to print 1 - 10, inclusively
-// WORK HERE
-
-for index in 1..<10 {
-    println("\(index)");
-
-}
-
-
 
 let worf = [
     "name": "Worf",
@@ -81,47 +54,52 @@ let picard = [
 
 let characters = [worf, picard]
 
-for (string1, string2) in worf {
-    println("\(string1): \(string2)" )
-
-}
-
-
 func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
     // WORK HERE
+    var drinksArray:[String] = []
     
-    return []
+    for character in characters {
+        drinksArray.append(character["favorite drink"]!)
+    }
+    
+    return drinksArray
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
 
-favoriteDrinks
-
-/*
-
-Functions
-
-*/
 
 // Make a function that inputs an array of strings and outputs the strings separated by a semicolon
 
 let strings = ["milk", "eggs", "bread", "challah"]
 
-func groceries(NSString)
-
-
 // WORK HERE - make your function and pass `strings` in
+func fromArrayToString(inArray: Array<String>) ->String {
+    var finalString : String = ""
+    
+    for var i = 0; i < inArray.count ; i++ {
+        finalString += inArray[i]
+        if (i < inArray.count - 1) {
+            finalString += ";"
+        }
+    }
+    
+    return finalString
+}
 
+let theOutput = fromArrayToString(strings)
 let expectedOutput = "milk;eggs;bread;challah"
 
 /*
-
 Closures
-
 */
 
 let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All Berries", "Cookie Crisp"]
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
+var cerealArraySortedAlphabetically = sorted(cerealArray, { (s1: String, s2: String) -> Bool in return s1 < s2 })
+cerealArraySortedAlphabetically
+
+// Non closure sort - so easy!
+var sortedNames = sorted(cerealArray, <)
